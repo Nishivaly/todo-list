9a8a5a1bc81c22972504cd52b993603872494522
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-31 23:38:43 -0300

    Modal for project

--- src/index.js
+++ src/index.js
@@ -2,40 +2,60 @@ import "./styles.css"
 import { createProject, createTodo, createProjectList } from "./todos"
 import { displayProjects } from "./elements"
 
-const myProjects = createProjectList();
-myProjects.addProject(createProject('Default project', 'notes here'));
-displayProjects(myProjects);
+const setup = (() => {
 
-const projectList = document.querySelector("#project-list");
-const newProjectBtn = document.querySelector('#new-project-btn')
-
-newProjectBtn.addEventListener('click', () => {
-    const project = createProject('Project title', 'project notes');
-    myProjects.addProject(project);
+    const myProjects = createProjectList();
+    myProjects.addProject(createProject('Default project', 'notes here'));
     displayProjects(myProjects);
-})
 
-projectList.addEventListener('click', event => {
-    if (event.target.classList.contains("delete-project")) {
-        
-        const index = event.target.closest('div').dataset.id;
-        myProjects.deleteProject(index);
+    const projectModal = document.querySelector('#project-modal');
+    const showProjectModal = document.querySelector('#open-project-modal');
+    const closeProjectModal = document.querySelector('#close-project-modal');
+    const projectForm = document.querySelector('#project-form');
+
+    showProjectModal.addEventListener('click', () => {
+        projectModal.showModal();
+    })
+
+    closeProjectModal.addEventListener('click', () => {
+        projectModal.close();
+    })
+
+    projectForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const title = document.querySelector('#project-title').value;
+        const notes = document.querySelector('#project-notes').value;
+        const project = createProject(title, notes);
+        myProjects.addProject(project);
+        projectForm.reset();
+        projectModal.close();
         displayProjects(myProjects);
-    }
+    })
 
-    if (event.target.classList.contains("delete-todo")) {
+    const projectList = document.querySelector("#project-list");
 
-        const i = event.target.closest('.project').dataset.id;
-        const j = event.target.closest('div').dataset.id;
-        myProjects.projects[i].deleteTodo(j);
-        displayProjects(myProjects);
-    }
+    projectList.addEventListener('click', event => {
+        if (event.target.classList.contains("delete-project")) {
 
-    if (event.target.classList.contains("add-todo")) {
+            const index = event.target.closest('div').dataset.id;
+            myProjects.deleteProject(index);
+            displayProjects(myProjects);
+        }
 
-        const index = event.target.closest('div').dataset.id;
-        myProjects.projects[index].addTodo(createTodo('todo title', "todo desc", 'duetomorrow', 'high priority'));
-        displayProjects(myProjects);
-    }
+        if (event.target.classList.contains("delete-todo")) {
 
-})
+            const i = event.target.closest('.project').dataset.id;
+            const j = event.target.closest('div').dataset.id;
+            myProjects.projects[i].deleteTodo(j);
+            displayProjects(myProjects);
+        }
+
+        if (event.target.classList.contains("add-todo")) {
+
+            const index = event.target.closest('div').dataset.id;
+            myProjects.projects[index].addTodo(createTodo('todo title', "todo desc", 'duetomorrow', 'high priority'));
+            displayProjects(myProjects);
+        }
+
+    })
+})();
