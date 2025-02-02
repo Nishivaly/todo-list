6198cc51649ddc7c1099122e4ba97e07c3a6b2ab
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-02-01 23:21:17 -0300

    Refactor edit todo functionality

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import "./styles.css"
 import { createProjectList } from "./factory"
 import { displayProjects } from "./display.js"
+import { setUpEditButtonEvent, setUpEditFormEvent, setUpTodoEditModalEvent } from "./todoedit.js";
 
 const myProjectList = createProjectList();
 const projects = myProjectList.getProjects();
 
 myProjectList.addProject('Default project', 'Cool notes about our shit idk')
-myProjectList.addTodo(0, 'idk', 'desc', 'due', 'ihgh');
+myProjectList.addTodo(0, 'idk', 'desc', '2025-02-04', 'High');
 myProjectList.getTodos(0)[0].title = 'urmwwwwm';
 
 displayProjects(projects);
 
-(() => {
+const eventSetup = (() => {
     const projectModal = document.querySelector('#project-modal');
     const showProjectModal = document.querySelector('#open-project-modal');
     const closeProjectModal = document.querySelector('#close-project-modal');
     const projectForm = document.querySelector('#project-form');
+
+    const todoModal = document.querySelector('#todo-modal');
+    const closeTodoModal = document.querySelector('#close-todo-modal');
+    const todoForm = document.querySelector('#todo-form');
+
+    // const todoEditModal = document.querySelector('#todo-edit-modal');
+    // const closeTodoEditModal = document.querySelector('#close-todo-edit-modal');
+    // const todoEditForm = document.querySelector('#todo-edit-form');
 
     showProjectModal.addEventListener('click', () => {
         projectModal.showModal();
@@ -76,28 +85,9 @@ displayProjects(projects);
 
         if (event.target.classList.contains("edit-todo")) {
 
-            const i = event.target.closest('.project').dataset.id;
-            const j = event.target.closest('.todo').dataset.id;
-
-            todoEditForm.dataset.indexProject = i;
-            todoEditForm.dataset.indexTodo = j;
-
-            const todo = myProjectList.getTodos(i)[j];
-            const { title, description, dueDate, priority } = todo;
-
-            document.querySelector('#todo-edit-title').value = title;
-            document.querySelector('#todo-edit-description').value = description;
-            document.querySelector('#todo-edit-due').value = dueDate;
-            document.querySelector('#todo-edit-priority').value = priority;
-
-            todoEditModal.showModal();
-
+            setUpEditButtonEvent(event, myProjectList);
         }
     })
-
-    const todoModal = document.querySelector('#todo-modal');
-    const closeTodoModal = document.querySelector('#close-todo-modal');
-    const todoForm = document.querySelector('#todo-form');
 
     closeTodoModal.addEventListener('click', () => {
         todoModal.close();
@@ -122,30 +112,8 @@ displayProjects(projects);
         displayProjects(projects);
     })
 
-    // Edit todo
-    const todoEditModal = document.querySelector('#todo-edit-modal');
-    const closeTodoEditModal = document.querySelector('#close-todo-edit-modal');
-    const todoEditForm = document.querySelector('#todo-edit-form');
+    setUpEditFormEvent(myProjectList, displayProjects);
 
-    closeTodoEditModal.addEventListener('click', () => {
-        todoEditModal.close();
-    })
+    setUpTodoEditModalEvent();
 
-    todoEditForm.addEventListener('submit', event => {
-        event.preventDefault();
-
-        const i = document.querySelector('#todo-edit-form').dataset.indexProject;
-        const j = document.querySelector('#todo-edit-form').dataset.indexTodo;
-
-        const todo = myProjectList.getTodos(i)[j];
-        todo.title = document.querySelector('#todo-edit-title').value;
-        todo.description = document.querySelector('#todo-edit-description').value;
-        todo.dueDate = document.querySelector('#todo-edit-due').value;
-        todo.priority = document.querySelector('#todo-edit-priority').value;
-
-        todoEditForm.reset();
-        todoEditModal.close();
-
-        displayProjects(projects);
-    })
 })();
---
+++ src/todoedit.js
@@ -0,0 +1,59 @@
+export const setUpEditButtonEvent = (event, myProjectList) => {
+
+    const i = event.target.closest('.project').dataset.id;
+    const j = event.target.closest('.todo').dataset.id;
+
+    elements.todoEditForm.dataset.indexProject = i;
+    elements.todoEditForm.dataset.indexTodo = j;
+
+    const todo = myProjectList.getTodos(i)[j];
+    const { title, description, dueDate, priority } = todo;
+
+    document.querySelector('#todo-edit-title').value = title;
+    document.querySelector('#todo-edit-description').value = description;
+    document.querySelector('#todo-edit-due').value = dueDate;
+    document.querySelector('#todo-edit-priority').value = priority;
+
+    elements.todoEditModal.showModal();
+}
+
+export const setUpEditFormEvent = (myProjectList, displayProjects) => {
+
+    elements.todoEditForm.addEventListener('submit', event => {
+        
+        event.preventDefault();
+
+        const i = document.querySelector('#todo-edit-form').dataset.indexProject;
+        const j = document.querySelector('#todo-edit-form').dataset.indexTodo;
+
+        const todo = myProjectList.getTodos(i)[j];
+
+        todo.title = document.querySelector('#todo-edit-title').value;
+        todo.description = document.querySelector('#todo-edit-description').value;
+        todo.dueDate = document.querySelector('#todo-edit-due').value;
+        todo.priority = document.querySelector('#todo-edit-priority').value;
+
+        elements.todoEditForm.reset();
+        elements.todoEditModal.close();
+
+        displayProjects(myProjectList.getProjects());
+    });
+}
+
+export const setUpTodoEditModalEvent = () => {
+    elements.closeTodoEditModal.addEventListener('click', () => {
+        elements.todoEditModal.close();
+    })
+};
+
+const elements = (() => {
+    const todoEditModal = document.querySelector('#todo-edit-modal');
+    const closeTodoEditModal = document.querySelector('#close-todo-edit-modal');
+    const todoEditForm = document.querySelector('#todo-edit-form');
+
+    return {
+        todoEditForm,
+        todoEditModal,
+        closeTodoEditModal
+    }
+})();
